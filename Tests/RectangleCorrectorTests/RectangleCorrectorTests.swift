2124147cde4ccea8d4620fe6b1dc51479a8ea19a
Diana Ma <mdianna1989@gmail.com>
2025-08-30 20:29:19 +0000

    reformat tests

--- Tests/RectangleCorrectorTests/RectangleCorrectorTests.swift
+++ Tests/RectangleCorrectorTests/RectangleCorrectorTests.swift
@@ -1,74 +1,77 @@
 import Testing
 @testable import RectangleCorrector
 
-@Test func functionCallFormatting() {
-    let input = "myFunction(arg1: 1, arg2: 2, arg3: 3, arg4: 4, arg5: 5, arg6: 6, arg7: 7, arg8: 8, arg9: 9, arg10: 10)"
-    let expected = """
-    myFunction(
-        arg1: 1,
-        arg2: 2,
-        arg3: 3,
-        arg4: 4,
-        arg5: 5,
-        arg6: 6,
-        arg7: 7,
-        arg8: 8,
-        arg9: 9,
-        arg10: 10
-    )
-    """
+@Suite struct RectangleCorrectorTests {
 
-    let actual = RectangleCorrector.correct(input, maxLength: 80)
+    @Test func FunctionCallFormatting() {
+        let input = "myFunction(arg1: 1, arg2: 2, arg3: 3, arg4: 4, arg5: 5, arg6: 6, arg7: 7, arg8: 8, arg9: 9, arg10: 10)"
+        let expected = """
+        myFunction(
+            arg1: 1,
+            arg2: 2,
+            arg3: 3,
+            arg4: 4,
+            arg5: 5,
+            arg6: 6,
+            arg7: 7,
+            arg8: 8,
+            arg9: 9,
+            arg10: 10
+        )
+        """
 
-    #expect(actual == expected)
-}
+        let actual = RectangleCorrector.correct(input, maxLength: 80)
 
-@Test func nestedFunctionCallFormatting() {
-    let input = "myFunction(arg1: anotherFunction(arg1: 1, arg2: 2), arg2: 3, arg3: 4, arg4: 5, arg5: 6, arg6: 7)"
-    let expected = """
-    myFunction(
-        arg1: anotherFunction(arg1: 1, arg2: 2),
-        arg2: 3,
-        arg3: 4,
-        arg4: 5,
-        arg5: 6,
-        arg6: 7
-    )
-    """
-
-    let actual = RectangleCorrector.correct(input, maxLength: 80)
-
-    #expect(actual == expected)
-}
-
-@Test func functionDeclarationFormatting() {
-    let input = "func myFunction(arg1: Int, arg2: String, arg3: Double, arg4: Bool, arg5: Int, arg6: String, arg7: Double) -> Void"
-    let expected = """
-    func myFunction(
-        arg1: Int,
-        arg2: String,
-        arg3: Double,
-        arg4: Bool,
-        arg5: Int,
-        arg6: String,
-        arg7: Double
-    ) -> Void
-    """
-
-    let actual = RectangleCorrector.correct(input, maxLength: 80)
-
-    #expect(actual == expected)
-}
-
-@Test func trailingClosureFormatting() {
-    let input = "myFunction(arg1: 1, arg2: 2, arg3: 3) { print(\"hello\") }"
-    let expected = """
-    myFunction(arg1: 1, arg2: 2, arg3: 3) {
-        print(\"hello\")
+        #expect(actual == expected)
     }
-    """
 
-    let actual = RectangleCorrector.correct(input, maxLength: 40)
+    @Test func NestedFunctionCallFormatting() {
+        let input = "myFunction(arg1: anotherFunction(arg1: 1, arg2: 2), arg2: 3, arg3: 4, arg4: 5, arg5: 6, arg6: 7)"
+        let expected = """
+        myFunction(
+            arg1: anotherFunction(arg1: 1, arg2: 2),
+            arg2: 3,
+            arg3: 4,
+            arg4: 5,
+            arg5: 6,
+            arg6: 7
+        )
+        """
 
-    #expect(actual == expected)
+        let actual = RectangleCorrector.correct(input, maxLength: 80)
+
+        #expect(actual == expected)
+    }
+
+    @Test func FunctionDeclarationFormatting() {
+        let input = "func myFunction(arg1: Int, arg2: String, arg3: Double, arg4: Bool, arg5: Int, arg6: String, arg7: Double) -> Void"
+        let expected = """
+        func myFunction(
+            arg1: Int,
+            arg2: String,
+            arg3: Double,
+            arg4: Bool,
+            arg5: Int,
+            arg6: String,
+            arg7: Double
+        ) -> Void
+        """
+
+        let actual = RectangleCorrector.correct(input, maxLength: 80)
+
+        #expect(actual == expected)
+    }
+
+    @Test func TrailingClosureFormatting() {
+        let input = "myFunction(arg1: 1, arg2: 2, arg3: 3) { print(\"hello\") }"
+        let expected = """
+        myFunction(arg1: 1, arg2: 2, arg3: 3) {
+            print(\"hello\")
+        }
+        """
+
+        let actual = RectangleCorrector.correct(input, maxLength: 40)
+
+        #expect(actual == expected)
+    }
 }
