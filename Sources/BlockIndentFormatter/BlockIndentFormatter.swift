22530d3156b43f2862267d9295a0a719dd02929e
Diana Ma <mdianna1989@gmail.com>
2025-09-06 20:26:30 +0000

    dogfood

--- Sources/BlockIndentFormatter/BlockIndentFormatter.swift
+++ Sources/BlockIndentFormatter/BlockIndentFormatter.swift
@@ -12,7 +12,10 @@ public struct BlockIndentFormatter {
                 source: current
             )
             // Post-processing step: Clean up any trailing whitespace on each line.
-            let lines: [Substring] = "\(rewriter.format())".split(separator: "\n", omittingEmptySubsequences: false)
+            let lines: [Substring] = "\(rewriter.format())".split(
+                separator: "\n",
+                omittingEmptySubsequences: false
+            )
             let formatted: String = lines.map { $0.trimmingWhitespaceFromEnd() }.joined(separator: "\n")
 
             if  formatted != current {
