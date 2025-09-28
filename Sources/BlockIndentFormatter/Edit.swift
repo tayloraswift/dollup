d78c9e8cf3e62234787044173a0f39c9b1b1dfe3
Diana <diana@rarestype.com>
2025-09-28 05:03:44 +0000

    lint and rename

--- Sources/BlockIndentFormatter/Edit.swift
+++
@@ -1,7 +0,0 @@
-import SwiftSyntax
-
-struct Edit {
-    let start: AbsolutePosition
-    let length: Int
-    let newText: String
-}
--- Sources/BlockIndentFormatter/Line.swift
+++ Sources/BlockIndentFormatter/Line.swift
@@ -1,4 +1,4 @@
-struct BlockIndentableLine {
+struct Line {
     /// UTF-8 byte offset where the first non-whitespace character appears.
     let start: Int
     /// The content of the line, with all leading whitespace and trailing whitespace removed.
