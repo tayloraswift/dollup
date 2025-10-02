9e8673abecea8ccf351ccc2878e96838526bf0f9
Dianna Ma <mdianna1989@gmail.com>
2025-10-02 02:10:06 +0000

    support in-memory use case

--- Sources/DollupSettings/Main.swift
+++ Sources/DollupSettings/Main.swift
@@ -3,10 +3,13 @@ import SystemPackage
 
 /// These are the settings Dollup uses to format itself.
 @main enum Main: DollupConfiguration {
-    public static func configure(_ settings: inout DollupSettings, file: FilePath) {
-        print("> '\(file)'")
+    public static func configure(_ settings: inout DollupSettings, file: FilePath?) {
         settings.whitespace {
             $0.width = 96
+        }
+
+        if  let file: FilePath {
+            print("> '\(file)'")
         }
     }
 }
