2519b8b3583464a5dc6f1d419d47dd2268681110
Diana <diana@rarestype.com>
2025-10-06 00:44:52 +0000

    make SwitchExpr indentation configurable, and implement handing indents for condition lists

--- Sources/WhitespaceFormatter/IndentOptions.swift
+++ Sources/WhitespaceFormatter/IndentOptions.swift
@@ -1,6 +1,10 @@
 public struct IndentOptions {
     /// Number of spaces per indentation level.
     public var spaces: Int
+    /// Whether to indent `case` blocks in `switch` expressions.
+    public var `switch`: Bool
     /// Whether to indent `#if`/`#else`/`#endif` blocks.
     public var ifConfig: Bool
+    /// Whether to hang alignable conditions in condition lists.
+    public var hangConditions: Bool
 }
